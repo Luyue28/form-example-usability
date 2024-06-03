b0966571c46b6ddb3149bead3cae8f3cd93985d8
Luyue28 <zhan0067@hz.nl>
2024-06-03 22:02:16 +0200

    done

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
 Route::get('/test500', [ExampleController::class, 'test500']);
+
+Route::get('/form', [App\Http\Controllers\StudentController::class, 'form'])->name('form');
+Route::post('/submit', [App\Http\Controllers\StudentController::class, 'store'])->name('student.store');
